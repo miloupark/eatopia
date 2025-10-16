876c477f52430cdd168047f3bde6979ae7e5a801
hyebinpark <hoonywhitesocks@gmail.com>
2025-10-17 00:46:56 +0900

    feat(app): useState로 로딩 상태 관리 및 데이터 로드 전 메시지 조건부 렌더링 구현

--- src/App.tsx
+++ src/App.tsx
@@ -16,14 +16,17 @@ import MainLayout from "./components/common/MainLayout";
 import { getPlaces, type Place } from "./api/place";
 
 function App() {
-  // 맛집 데이터 상태 관리
+  // 상태 관리
   const [places, setPlaces] = useState<Place[]>([]);
+  const [loading, setLoading] = useState(true);
 
   // 컴포넌트 마운트 시, API 요청
   useEffect(() => {
+    setLoading(true); // 요청 시작 시 로딩 시작
     getPlaces()
       .then((res) => setPlaces(res))
-      .catch((err) => console.error("맛집 데이터를 불러오지 못했습니다.", err));
+      .catch((err) => console.error("맛집 데이터를 불러오지 못했습니다.", err))
+      .finally(() => setLoading(false)); // 성공/실패 상관없이 로딩 종료
   }, []);
 
   return (
@@ -33,17 +36,22 @@ function App() {
         {/* 맛집 목록 */}
         <section>
           <h2 className="text-center">맛집 목록</h2>
-          <div className="grid grid-cols-4 gap-5 p-10">
-            {places.map((place) => (
-              <Card
-                id={place.id}
-                key={place.id}
-                title={place.title}
-                image={place.image}
-                description={place.description}
-              />
-            ))}
-          </div>
+
+          {loading ? (
+            <p>🍽️ 맛집을 불러오는 중입니다...</p>
+          ) : (
+            <div className="grid grid-cols-4 gap-5 p-10">
+              {places.map((place) => (
+                <Card
+                  id={place.id}
+                  key={place.id}
+                  title={place.title}
+                  image={place.image}
+                  description={place.description}
+                />
+              ))}
+            </div>
+          )}
         </section>
 
         {/* 찜 맛집 목록 */}
